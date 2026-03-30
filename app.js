22202e61ad96f003744e1aaf7ef7f829854b8e16
Annabel <101633619+Angel-Hime@users.noreply.github.com>
2026-03-30 12:27:47 +0100

    Fix audio element volume assignment

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-const sFTrack = document.createElement("audio").volume = 0.5;
+const sFTrack = document.createElement("audio");
 
 sFTrack.src = "./audio/Smiling-Friends-Intro.mp4";
-
+sFTrack.volume = 0.3;
 sFTrack.play();
-
-
